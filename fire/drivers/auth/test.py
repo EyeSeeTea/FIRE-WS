25c4fbe6b184f4faf1dd3dfdb664dfc75477451b
Arnau Sanchez <pyarnau@gmail.com>
2017-10-02 11:07:50 +0200

    Add AuthInterface.add_user interface + kamailio implementation

--- fire/drivers/auth/test.py
+++ fire/drivers/auth/test.py
@@ -3,3 +3,6 @@ from fire.engine.interfaces import AuthInterface
 class AuthTest(AuthInterface):
     def get_password(self, username):
         return username + "pass"
+
+    def add_user(self, username, password):
+        return True
--- fire/tools.py
+++ fire/tools.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import subprocess
 
 def first(it):
     return next(it, None)
@@ -8,13 +10,24 @@ def merge(d1, d2):
     d3.update(d2)
     return d3
 
-def create_logger(name):
-    formatter = logging.Formatter(fmt='[%(levelname)s:%(module)s] %(message)s')
+def create_logger(name, level=logging.DEBUG, format='[%(levelname)s:%(module)s] %(message)s'):
+    formatter = logging.Formatter(fmt=format)
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
     logger.addHandler(handler)
     return logger
+
+def run(cmd, env=None):
+    logger.debug("Run: {}".format(cmd))
+    my_env = os.environ.copy()
+    for key, value in (env or {}).items():
+        my_env[key] = value
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=my_env)
+    if result.returncode:
+        logger.debug("Error, returncode {}:\nstdout={}\nstderr={}".\
+            format(result.returncode, result.stdout, result.stderr))
+    return result
 
 logger = create_logger("fire")
